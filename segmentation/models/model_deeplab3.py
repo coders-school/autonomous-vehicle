fa98b6c606b873baacb1d79a7e682bfacb84df4c
Michał Maj <michalpmaj@gmail.com>
2024-02-07 15:04:28 +0100

    Building basic network

--- segmentation/models/model_deeplab3.py
+++ segmentation/models/model_deeplab3.py
@@ -17,3 +17,18 @@ class aspp_conv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+
+class aspp_pool(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(aspp_pool, self).__init__()
+        self.pooling_module = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU())
+
+    def forward(self, x):
+        input_size = x.shape[-2:]
+        x = self.pooling_module(x)
+        return F.interpolate(x, size=input_size, mode='bilinear', align_corners=False)
