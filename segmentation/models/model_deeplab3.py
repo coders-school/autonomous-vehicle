675a910492f45896da8c07f054747d0d76b4d642
Michał Maj <michalpmaj@gmail.com>
2024-02-07 15:08:59 +0100

    Adding Spatial Pyramid Pooling

--- segmentation/models/model_deeplab3.py
+++ segmentation/models/model_deeplab3.py
@@ -32,3 +32,39 @@ class aspp_pool(nn.Module):
         input_size = x.shape[-2:]
         x = self.pooling_module(x)
         return F.interpolate(x, size=input_size, mode='bilinear', align_corners=False)
+        
+
+class atrous_spatial_pyramid_pooling(nn.Module):
+    def __init__(self, in_channels, out_channels, dilation_rates):
+        super(atrous_spatial_pyramid_pooling, self).__init__()
+
+        layers = nn.ModuleList([])
+
+        layers.append(nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        ))
+
+        for rate in dilation_rates:
+            layers.append(aspp_conv(in_channels, out_channels, rate))
+
+        layers.append(aspp_pool(in_channels, out_channels))
+
+        self.layers = nn.ModuleList(layers)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(len(layers) * out_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, x):
+        conv_outputs = []
+        for mod in self.layers:
+            mod_output = mod(x)
+            conv_outputs.append(mod_output)
+
+        output = self.project(torch.cat(conv_outputs, dim=1))
+        return output
