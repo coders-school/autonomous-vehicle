9ecdef0b9cf8ecb6ad213310725061f28a4dbbc8
Michał Maj <michalpmaj@gmail.com>
2024-02-08 06:31:42 +0100

    Adding Full DeepLabv3+ Module

--- segmentation/models/model_deeplab3.py
+++ segmentation/models/model_deeplab3.py
@@ -1,10 +1,12 @@
 # Define deeplabv3+ model using resnet50, Atrous convolutions, ASPP modules, Decoder
 
 # Import PyTorch
-# DL library imports
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+# Import ResNet50 as backbone
+from torchvision.models import resnet50
 
 
 class aspp_conv(nn.Module):
@@ -96,3 +98,47 @@ class deeplabv3_decoder(nn.Module):
 
         x = self.cls(x)
         return x
+        
+        
+class deeplabv3_plus(nn.Module):
+    def __init__(self, in_channels, output_stride, num_classes):
+        super(deeplabv3_plus, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.output_stride = output_stride
+
+        if (output_stride == 16):
+            dilation_rates = [6, 12, 18]
+            replace_stride_with_dilation = [False, False, True]
+
+        elif (output_stride == 8):
+            dilation_rates = [12, 24, 36]
+            replace_stride_with_dilation = [False, True, True]
+
+        backbone = resnet50(pretrained=True, replace_stride_with_dilation=replace_stride_with_dilation)
+        self.initial = nn.Sequential(*list(backbone.children())[:4])
+        self.layer1 = backbone.layer1
+        self.layer2 = backbone.layer2
+        self.layer3 = backbone.layer3
+        self.layer4 = backbone.layer4
+
+        aspp_out_channels = 256
+        aspp_in_channels = int(backbone.fc.in_features)
+        self.aspp_module = atrous_spatial_pyramid_pooling(aspp_in_channels,
+                                                          out_channels=aspp_out_channels, dilation_rates=dilation_rates)
+
+        self.decoder = deeplabv3_decoder(self.num_classes)
+
+    def forward(self, x):
+        input_size = x.shape[-2:]
+
+        x = self.initial(x)
+        low_level_feat = self.layer1(x)
+        x = self.layer2(low_level_feat)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        aspp_output = self.aspp_module(x)
+        decoder_output = self.decoder(aspp_output, low_level_feat)
+        return F.interpolate(decoder_output, size=input_size, mode='bilinear', align_corners=False)
+        
