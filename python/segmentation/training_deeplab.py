c413569a06476ea15d819352e356f15cef99885b
Michał Maj <michalpmaj@gmail.com>
2024-02-15 11:17:40 +0100

    Changing train_validate_model to wrapper version

--- python/segmentation/training_deeplab.py
+++ python/segmentation/training_deeplab.py
@@ -7,6 +7,7 @@ from torch.optim.lr_scheduler import OneCycleLR
 import segmentation_models_pytorch as smp
 
 from utils import meanIoU
+from utils import train_validate_model
 
 criterion = smp.losses.DiceLoss('multiclass', classes=[0,1,2], log_loss = True, smooth=1.0)
 
@@ -16,6 +17,6 @@ optimizer = optim.Adam(model.parameters(), lr=MAX_LR)
 scheduler = OneCycleLR(optimizer, max_lr= MAX_LR, epochs = N_EPOCHS,steps_per_epoch = len(train_dataloader),
                        pct_start=0.3, div_factor=10, anneal_strategy='cos')
 
-_ = hp.train_validate_model(model, N_EPOCHS, MODEL_NAME, criterion, optimizer,
+_ = train_validate_model(model, N_EPOCHS, MODEL_NAME, criterion, optimizer,
                          device, train_dataloader, val_dataloader, meanIoU, 'meanIoU',
                          NUM_CLASSES, lr_scheduler = scheduler, output_path = output_path)
