9ab52f3d9ce744b2d23cf18e5909c94e7b4403a9
Michał Maj <michalpmaj@gmail.com>
2024-02-26 21:13:01 +0100

    Adding Efficient Self Attention

--- python/segmentation/models/model_transformer.py
+++ python/segmentation/models/model_transformer.py
@@ -46,3 +46,50 @@ class mix_feedforward(nn.Module):
         x = F.dropout(x, p=self.dropout_p, training=self.training)
         return x
     
+    
+class efficient_self_attention(nn.Module):
+    def __init__(self, attn_dim, num_heads, dropout_p, sr_ratio):
+        super().__init__()
+        assert attn_dim % num_heads == 0, f'expected attn_dim {attn_dim} to be a multiple of num_heads {num_heads}'
+        self.attn_dim = attn_dim
+        self.num_heads = num_heads
+        self.dropout_p = dropout_p
+        self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(attn_dim, attn_dim, kernel_size=sr_ratio, stride=sr_ratio)
+            self.norm = nn.LayerNorm(attn_dim)
+
+        # Multi-head Self-Attention using dot product
+        # Query - Key Dot product is scaled by root of head_dim
+        self.q = nn.Linear(attn_dim, attn_dim, bias=True)
+        self.kv = nn.Linear(attn_dim, attn_dim * 2, bias=True)
+        self.scale = (attn_dim // num_heads) ** -0.5
+
+        # Projecting concatenated outputs from 
+        # multiple heads to single `attn_dim` size
+        self.proj = nn.Linear(attn_dim, attn_dim)
+
+
+    def forward(self, x, h, w):
+        q = self.q(x)
+        q = rearrange(q, ('b hw (m c) -> b m hw c'), m=self.num_heads)
+
+        if self.sr_ratio > 1:
+            x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+            x = self.sr(x)
+            x = rearrange(x, 'b c h w -> b (h w) c')
+            x = self.norm(x)
+
+        x = self.kv(x)
+        x = rearrange(x, 'b d (a m c) -> a b m d c', a=2, m=self.num_heads)
+        k, v = x[0], x[1] # x.unbind(0)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+
+        x = attn @ v
+        x = rearrange(x, 'b m hw c -> b hw (m c)')
+        x = self.proj(x)
+        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        return x
+        
