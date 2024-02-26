9f2a299719ebcc5aaaccde7b1197ce669c2a2958
Michał Maj <michalpmaj@gmail.com>
2024-02-26 20:41:14 +0100

    Adding Mix Feed Forward Network

--- python/segmentation/models/model_transformer.py
+++ python/segmentation/models/model_transformer.py
@@ -22,3 +22,27 @@ class overlap_patch_embed(nn.Module):
         x = rearrange(x, 'b c h w -> b (h w) c')
         x = self.norm(x)
         return x, h, w
+        
+        
+class mix_feedforward(nn.Module):
+    def __init__(self, in_features, out_features, hidden_features, dropout_p=0.0):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.fc2 = nn.Linear(hidden_features, out_features)
+
+        # Depth-wise separable convolution
+        self.conv = nn.Conv2d(hidden_features, hidden_features, (3, 3), padding=(1, 1),
+                              bias=True, groups=hidden_features)
+        self.dropout_p = dropout_p
+
+    def forward(self, x, h, w):
+        x = self.fc1(x)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+        x = self.conv(x)
+        x = rearrange(x, 'b c h w -> b (h w) c')
+        x = F.gelu(x)
+        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        x = self.fc2(x)
+        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        return x
+    
