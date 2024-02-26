bd7a9ff5706bd956583b1416e07fbc1be6478629
Michał Maj <michalpmaj@gmail.com>
2024-02-26 20:39:37 +0100

    Define a Segformer MiT B3 model with Mix Transformer encoder and MLP decoder

---
+++ python/segmentation/models/model_transformer.py
@@ -0,0 +1,24 @@
+# DL library imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from einops import rearrange
+import segmentation_models_pytorch as smp
+from timm.models.layers import drop_path, trunc_normal_
+
+
+class overlap_patch_embed(nn.Module):
+    def __init__(self, patch_size, stride, in_chans, embed_dim):
+        super().__init__()
+        self.patch_size = patch_size
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
+                              padding=(patch_size // 2, patch_size // 2))
+        self.norm = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, h, w = x.shape
+        x = rearrange(x, 'b c h w -> b (h w) c')
+        x = self.norm(x)
+        return x, h, w
